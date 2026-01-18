73084f8856828dda390fbbc4a78570965c1f102a
عبدالرحمن النوفلي <abdoooo99@gmail.com>
2026-01-18 16:21:40 +0000

    ظهرت الخلفية سوداء بدون صورة

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -22,7 +22,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
   return (
     <div 
         className="relative h-screen w-screen items-center justify-center bg-cover bg-center text-white" 
-        style={{ backgroundImage: "url('https://images.unsplash.com/photo-1582276524357-526365313413?q=80&w=1920&auto=format&fit=crop')" }}
+        style={{ backgroundImage: "url('https://images.unsplash.com/photo-1591348122299-4684b5536484?q=80&w=1974&auto=format&fit=crop')" }}
         data-ai-hint="grand mosque"
         dir={dir}
     >
